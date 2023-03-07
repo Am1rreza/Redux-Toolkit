70f6f2e738f5ed05511870f3d7d8f66c70e56505
Amirreza <amirreza.m13825@gmail.com>
2023-03-07 22:51:31 +0330

    increment and decrement by an arbitrary number

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "../features/counter/CounterSlice";
 
 const CounterComponent = () => {
+  const [inputValue, setInputValue] = useState("");
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
@@ -9,8 +11,17 @@ const CounterComponent = () => {
     <div>
       <h1>Counter State</h1>
       <h2>Counter : {counter.value}</h2>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={() => dispatch(increment(Number(inputValue) || 1))}>
+        +
+      </button>
+      <button onClick={() => dispatch(decrement(Number(inputValue) || 1))}>
+        -
+      </button>
     </div>
   );
 };
