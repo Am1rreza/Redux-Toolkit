0f5bccfc70e67a381df2efb68a2fdc2bdec02dd5
Amirreza <amirreza.m13825@gmail.com>
2023-03-07 21:53:11 +0330

    create CounterComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { store } from "./features/store";
 import { Provider } from "react-redux";
+import CounterComponent from "./components/CounterComponent";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App"></div>
+      <div className="App">
+        <CounterComponent />
+      </div>
     </Provider>
   );
 }
---
+++ src/components/CounterComponent.jsx
@@ -0,0 +1,12 @@
+const CounterComponent = () => {
+  return (
+    <div>
+      <h1>Counter State</h1>
+      <h2>Counter :</h2>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+};
+
+export default CounterComponent;
