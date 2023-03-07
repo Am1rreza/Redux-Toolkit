ba8a6bf87225c8d4339a1d31af3cb12371c6d4f5
Amirreza <amirreza.m13825@gmail.com>
2023-03-07 23:43:13 +0330

    create todosSlice with redux toolkit

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: 1, title: "todo1", completed: false },
+  { id: 2, title: "todo2", completed: false },
+  { id: 3, title: "todo3", completed: true },
+];
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: new Date().getTime(),
+        title: action.payload.title,
+        isCompleted: false,
+      };
+
+      state.push(newTodo);
+    },
+  },
+});
+
+export const { addTodo } = todosSlice.actions;
+export default todosSlice.reducer;
