7987ec8b3c3b2b8e043f4827043f28b912d31680
Amirreza <amirreza.m13825@gmail.com>
2023-03-07 23:31:29 +0330

    Todo-List project initialized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { store } from "./features/store";
-import { Provider } from "react-redux";
-import CounterComponent from "./components/CounterComponent";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteTodo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <CounterComponent />
-      </div>
-    </Provider>
+    <div className="App container">
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
+    </div>
   );
 }
 
