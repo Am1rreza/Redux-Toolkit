8f3f66cdf363c03eede274c8b05bd66f349572ff
Amirreza <amirreza.m13825@gmail.com>
2023-03-07 22:07:37 +0330

    add CounterComponent logic

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -1,10 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../features/counter/CounterSlice";
+
 const CounterComponent = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Counter State</h1>
-      <h2>Counter :</h2>
-      <button>+</button>
-      <button>-</button>
+      <h2>Counter : {counter.value}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 };
